fedfece71bb38ae6ddb59e534d132b7b09b0f8d3
Nathan McIntosh <Nathan.McIntosh@jhuapl.edu>
2020-12-01 09:00:03 -0500

    day 1 complete

---
+++ day1.jl
@@ -0,0 +1,25 @@
+using Combinatorics
+
+function test_data_1()
+    [1721, 979, 366, 299, 675, 1456]
+end
+
+read_input(filename) = filename |> readlines .|> x -> parse(Int, x)
+
+function part1(input_numbers)
+    only(x*y for (x, y) in combinations(input_numbers, 2) if x+y == 2020)
+end
+
+function part2(input_numbers)
+    only(x*y*z for (x, y, z) in combinations(input_numbers, 3) if x+y+z == 2020)
+end
+
+# Script
+filename = "inputs/day1.txt"
+input_numbers = read_input(filename)
+
+part1_solution = part1(input_numbers)
+@show part1_solution
+
+part2_solution = part2(input_numbers)
+@show part2_solution
