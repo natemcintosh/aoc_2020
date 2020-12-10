03cedb167453a7623ec959fc5812a2d3224b88cd
Nathan McIntosh <Nathan.McIntosh@jhuapl.edu>
2020-12-10 13:12:27 -0500

    better comments explaning algorithm

--- day10.jl
+++ day10.jl
@@ -20,12 +20,10 @@ function counter(data::AbstractArray{T}) where T
 end
 
 """
-part2 works by knowing that the number of paths we can take to get to any individual
-adapter is the sum of the possible ways that that adapter can be reached by those within
-3 steps before it
+part2 works by knowing that the number of paths to get to any individual adapter is the
+sum of the possible paths to each of the three potentially reachable adapters before it
 """
 function part2(adapters)
-    # adapters = vcat([0, maximum(adapters) + 3], adapters) |> sort
     n = length(adapters)
     different_paths = zeros(Int, n)
     different_paths[1] = 1
@@ -40,8 +38,8 @@ function part2(adapters)
             item_diff = adapters[i+j]-adapters[i]
 
             # If it's > 3, not reachable.
-            # If it's <= 3, we can reach the item at i+j. Add the number of ways to get
-            # to item i to the number at ij
+            # If it's <= 3, we can reach the item at i+j from i. 
+            # Add the number of ways to get to item i to the number at i+j
             if item_diff <= 3
                 different_paths[i+j] += different_paths[i]
             end
@@ -54,10 +52,10 @@ end
 
 
 @time begin
-    adapters = "inputs/day10.txt" |> 
-        readlines |> 
-        x -> parse.(Int, x) |> 
-        x -> vcat([0, maximum(x) + 3], x) |> 
+    adapters = "inputs/day10.txt" |>
+        readlines |>
+        x -> parse.(Int, x) |>
+        x -> vcat([0, maximum(x) + 3], x) |>
         sort
     part1_solution = part1(adapters)
     @show part1_solution
